fc4a5b632ffc96c1ae112a79b15016c769540ec8
thanush <thanushsiva06@gmail.com>
2022-06-21 11:05:23 +0530

    child to parent communication

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,10 +1,19 @@
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css'
 
-function NewExpense(){
+function NewExpense(props) {
+    const saveExpenseDataHandler = (enteredExpenseDate) => {
+        const expenseData = {
+            ...enteredExpenseDate,
+            id: Math.random().toString()
+        }
+        // console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
+    }
+
     return (
         <div className='new-expense'>
-            <ExpenseForm/>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
         </div>
     );
 }
