aba349f0d3f24d6bbf3f9d64293845436c5b42d1
thanush <thanushsiva06@gmail.com>
2022-06-21 09:49:17 +0530

    working with multipe state

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,8 +1,18 @@
 import './ExpenseForm.css'
 
 function ExpenseForm() {
+    const [enteredTitle, setEnteredTitle] = ueState('');
+    const [enteredAmount, setEnteredAmount] = ueState('');
+    const [enteredDate, setEnteredDate] = ueState('');
+    
     const titleChangeHandler = (event) => {
-        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+    };
+    const amountChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    };
+    const dateChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
     };
 
     return (
@@ -14,11 +24,11 @@ function ExpenseForm() {
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01"/>
+                    <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31"/>
+                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
